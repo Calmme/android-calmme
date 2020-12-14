1ff7b20cbf4a67567025f54aab2ad7b86e630ab5
Codejune <kbj9704@gmail.com>
2020-12-15 04:04:14 +0900

    카메라기반 심박수 확인 프로토타입 포팅

--- app/src/main/java/kr/co/mooreung/fragment/HomeFragment.kt
+++ app/src/main/java/kr/co/mooreung/fragment/HomeFragment.kt
@@ -1,11 +1,17 @@
 package kr.co.mooreung.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 import kr.co.mooreung.R
+import kr.co.mooreung.activity.HeartScanActivity
+import kr.co.mooreung.activity.LoginActivity
 
 class HomeFragment : Fragment() {
 
@@ -15,5 +21,13 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // 로그아웃 버튼
+        heartScan.setOnClickListener { view ->
+            startActivity(Intent(context, HeartScanActivity::class.java))
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
 }
