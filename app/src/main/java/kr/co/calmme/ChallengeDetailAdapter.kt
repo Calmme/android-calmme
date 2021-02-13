2b249838d246630b4a1baeb013e989caaed43361
유찬양 <clsrnthd7@gmail.com>
2021-02-13 17:07:54 +0900

    챌린지 탭 디테일 화면 구성

---
+++ app/src/main/java/kr/co/calmme/ChallengeDetailAdapter.kt
@@ -0,0 +1,4 @@
+package kr.co.calmme
+
+class ChallengeDetailAdapter {
+}
---
+++ app/src/main/java/kr/co/calmme/activity/ChallengeDetailActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.calmme.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChallengeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_detail)
+    }
+}
