888aeeccd145230c5af04f7540758717165d5e65
유찬양 <clsrnthd7@gmail.com>
2021-02-13 17:09:04 +0900

    챌린지탭 추가 파일

--- app/src/main/java/kr/co/calmme/ChallengeDetailAdapter.kt
+++ app/src/main/java/kr/co/calmme/ChallengeDetailAdapter.kt
@@ -1,4 +1,53 @@
 package kr.co.calmme
 
-class ChallengeDetailAdapter {
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ChallengeDetailAdapter(val items: ArrayList<ChallengeDetailData>) :  RecyclerView.Adapter<ChallengeDetailAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ChallengeDetailAdapter.ViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_challenge_detail, parent, false)
+        val viewHolder = ViewHolder(view)
+        return viewHolder
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val description = itemView.findViewById<TextView>(R.id.challenge_item_text)
+        val jelly = itemView.findViewById<ImageView>(R.id.challenge_item_jelly)
+        val icon = itemView.findViewById<ImageView>(R.id.challenge_item_icon)
+        val layout = itemView.findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.challenge_item_layout)
+    }
+
+    override fun onBindViewHolder(holder: ChallengeDetailAdapter.ViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.layout.setOnClickListener {
+            holder.icon.setImageResource(R.drawable.ic_challenge_play)
+            holder.layout.setBackgroundColor(Color.parseColor("#806E42"))
+            holder.description.setTextColor(Color.parseColor("#FFFFFF"))
+            holder.jelly.setImageResource(R.drawable.ic_challenge_full_jelly)
+        }
+        holder.description.text = item.description
+        if(item.lock) {
+            holder.icon.setImageResource(R.drawable.ic_challenge_lock)
+            holder.description.setTextColor(Color.parseColor("#757575"))
+        }
+
+        if(item.background == "lightBlack")
+            holder.layout.setBackgroundResource(R.color.lightBlack)
+    }
+
+    override fun getItemCount(): Int = items.size
+
 }
+class ChallengeDetailData(val description: String, val background: String, val lock: Boolean)
--- app/src/main/java/kr/co/calmme/activity/ChallengeDetailActivity.kt
+++ app/src/main/java/kr/co/calmme/activity/ChallengeDetailActivity.kt
@@ -1,11 +1,91 @@
 package kr.co.calmme.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.skydoves.balloon.*
+import kotlinx.android.synthetic.main.activity_challenge_detail.*
+import kr.co.calmme.ChallengeDetailAdapter
+import kr.co.calmme.ChallengeDetailData
+import kr.co.calmme.R
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ChallengeDetailActivity : AppCompatActivity() {
+class ChallengeDetailActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_challenge_detail)
+
+        findViewById<ImageView>(R.id.challenge_detail_back).setOnClickListener(this)
+        findViewById<ImageView>(R.id.challenge_detail_tip).setOnClickListener(this)
+        setRecylerView()
+        setProgressBar()
+    }
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.challenge_detail_back -> finish()
+            R.id.challenge_detail_tip -> showTip(v)
+        }
+    }
+
+    fun showTip(view: View){
+        val balloon = createBalloon(this) {
+            setWidth(170)
+            setTextSize(9f)
+            setArrowSize(10)
+            setPadding(7)
+            setText("#우울할때 #힘들때 #사과먹고싶을때\n\n" + " ‘3분 나를 위한 명상’은 일하다 우울할 때나 울고 싶을때 감정이 힘들 때 들으면 효과 좋은 명상이에요")
+            setArrowOrientation(ArrowOrientation.TOP)
+            setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+            setArrowPosition(0.5f)
+            setCornerRadius(4f)
+            setElevation(6)
+
+            setBackgroundColorResource(R.color.originBlack)
+            setBalloonAnimation(BalloonAnimation.FADE)
+            setDismissWhenTouchOutside(true)
+            setDismissWhenShowAgain(true)
+        }
+
+        balloon.showAlignBottom(view)
+    }
+
+    fun setRecylerView(){
+        val items = ArrayList<ChallengeDetailData>()
+
+        items.add(ChallengeDetailData("1일차, 3분 나를 위한 명상", "Grey", false))
+        items.add(ChallengeDetailData("2일차, 3분 나를 위한 명상", "lightBlack", true))
+        items.add(ChallengeDetailData("2일차, 3분 나를 위한 명상", "lightBlack", true))
+        items.add(ChallengeDetailData("2일차, 3분 나를 위한 명상", "lightBlack", true))
+        items.add(ChallengeDetailData("2일차, 3분 나를 위한 명상", "lightBlack", true))
+        items.add(ChallengeDetailData("2일차, 3분 나를 위한 명상", "lightBlack", true))
+
+        val ChallengeDetailAdapter = ChallengeDetailAdapter(items)
+        val recyclerView = findViewById<RecyclerView>(R.id.challenge_detail_recyler)
+        recyclerView.adapter = ChallengeDetailAdapter
+        val lm = LinearLayoutManager(this)
+        recyclerView.layoutManager = lm
+    }
+
+    fun setProgressBar(){
+        val progress = intArrayOf(
+            R.id.challenge_detail_progress1,
+            R.id.challenge_detail_progress2,
+            R.id.challenge_detail_progress3,
+            R.id.challenge_detail_progress4,
+            R.id.challenge_detail_progress5,
+            R.id.challenge_detail_progress6,
+            R.id.challenge_detail_progress7
+        )
+
+        for(i in 0..5){
+            findViewById<ImageView>(progress[i]).setBackgroundColor(Color.parseColor("#FFDF8E"))
+        }
+
     }
 }
